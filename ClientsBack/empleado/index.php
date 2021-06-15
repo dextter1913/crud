5cfe17eacf97e49fdb5ec165215f5ae81a3943ff
dextter1913 <dextter1913@gmail.com>
2021-06-15 15:22:41 -0500

    Agregando metodo insertarEmpleado

--- ClientsBack/empleado/index.php
+++ ClientsBack/empleado/index.php
@@ -5,8 +5,8 @@ switch ($_SERVER['REQUEST_METHOD']) {
     case 'POST':
         $ContenidoHeader = file_get_contents(('php://input'), true);
         $_POST = json_decode($ContenidoHeader);
-        
-        
+        $insertarEmpleado = new InsertEmpleado($_POST['nombre'],$_POST['apellido'],$_POST['telefono']);
+        $insertarEmpleado->InsertarEmpleado();
         break;
     case 'PUT':
         # code...
