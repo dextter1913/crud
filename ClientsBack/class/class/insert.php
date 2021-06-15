2a2493d61e7b6579cfe89ac7f3cc3cb2b7b44df0
dextter1913 <dextter1913@gmail.com>
2021-06-15 15:20:35 -0500

    Agregando clase insertar empleado

--- ClientsBack/class/class/insert.php
+++ ClientsBack/class/class/insert.php
@@ -27,3 +27,28 @@ class insert
         http_response_code(201);
     }
 }
+
+class InsertEmpleado  
+{
+    private $nombre;
+    private $apellido;
+    private $telefono;
+    
+    public function __construct($nombre, $apellido, $telefono) {
+        $this->nombre = $nombre;
+        $this->apellido = $apellido;
+        $this->telefono = $telefono;
+    }
+
+    public function InsertarEmpleado(){
+        $nombre = $this->nombre;
+        $apellido = $this->apellido;
+        $telefono = $this->telefono;
+
+        $conexion = new conexion();
+        $conexion->EstablecerConexion()->query("INSERT INTO empleado(nombre, apellido, telefono) VALUES('$nombre','$apellido','$telefono')");
+        print 'Creado';
+        http_response_code(201);
+    }
+}
+
--- ClientsBack/empleado/index.php
+++ ClientsBack/empleado/index.php
@@ -1,9 +1,11 @@
 <?php
+require_once '../class/autoload.php';
 header("Content-Type: JSON");
 switch ($_SERVER['REQUEST_METHOD']) {
     case 'POST':
         $ContenidoHeader = file_get_contents(('php://input'), true);
         $_POST = json_decode($ContenidoHeader);
+        
         
         break;
     case 'PUT':
