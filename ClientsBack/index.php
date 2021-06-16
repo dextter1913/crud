9fb1b76eb5c5e8a2a4eea5f8046e59c69cfdfa98
dextter1913 <dextter1913@gmail.com>
2021-06-15 22:01:55 -0500

    Agregando Conversor de imagen

--- ClientsBack/index.php
+++ ClientsBack/index.php
@@ -11,7 +11,7 @@ switch ($_SERVER['REQUEST_METHOD']) {
             //intval($_POST['idEmpleado']);
             $insert = new insert($_POST['nombre'], $_POST['apellido'], $_POST['telefono'], $_POST['idEmpleado']);
             $insert->insertar();
-        }else {
+        } else {
             echo "Datos requeridos faltantes";
             http_response_code(406);
         }
@@ -38,7 +38,7 @@ switch ($_SERVER['REQUEST_METHOD']) {
                 $update->updateTelefono();
                 $T++;
             }
-        } 
+        }
         break;
     case 'DELETE':
         $_DELETE = json_decode(file_get_contents('php://input'), true);
@@ -46,38 +46,74 @@ switch ($_SERVER['REQUEST_METHOD']) {
         $delete->Eliminar();
         break;
     case 'GET':
-        if (isset($_GET['id'])) {
-            $respuesta = new select($_GET['id']);
-            $conexion = new conexion();
-            $resultado = mysqli_query($conexion->EstablecerConexion(), $respuesta->query());
-            while ($rows = mysqli_fetch_assoc($resultado)) {
-                $userData = $rows;
-            }
-            if (empty($userData)) {
-                echo 'no existe';
-                http_response_code(404);
-            } else {
-                echo json_encode($userData);
-            }
-        } else {
-            $respuesta = new select($_GET['id'] = null);
-            $conexion = new conexion();
-            $resultado = mysqli_query($conexion->EstablecerConexion(), $respuesta->sinid());
-            $i = 0;
-            while ($rows = mysqli_fetch_assoc($resultado)) {
-                $userData[$i]['id'] = $rows['id'];
-                $userData[$i]['nombre'] = $rows['nombre'];
-                $userData[$i]['apellido'] = $rows['apellido'];
-                $userData[$i]['telefono'] = $rows['telefono'];
-                $i++;
-            }
-            if (empty($userData)) {
-                echo "No Existen datos";
-                http_response_code(404);
-            }else {
-                echo json_encode($userData, JSON_PRETTY_PRINT);
-            }
-        }
+        header("Content-Type: JSON");
+        $IMAGEN = json_decode(file_get_contents('php://input'), true);
+
+        //convirtiendo y guardando imagen traida por el json
+        $rutaImagenSalida = __DIR__ . "/ficheros/{$IMAGEN['name']}";
+        $convertido = base64_decode($IMAGEN['base']);
+        $imagenfinal = file_put_contents($rutaImagenSalida, $convertido);
+
+        //devolviendo imagen en json
+        // Nombre de la imagen
+        $path = 'ficheros/tax.jpg';
+        $type = pathinfo($path, PATHINFO_EXTENSION);
+        $data = file_get_contents($path);
+        $base64['base'] = 'data:image/' . $type . ';base64,' . base64_encode($data);
+        echo json_encode($base64);
+
+
+
+
+        //$archivo = fopen("ficheros/imagen.jpg", "a+");
+        //$convertido = base64_encode($IMAGEN['base']);
+        //
+        //fwrite($archivo, $convertido);
+        //
+        //fclose($archivo);
+        //$archivo = fopen("ficheros/imagen.jpg", "a+");
+        //$convertido = base64_decode($archivo);
+        //while (!feof($convertido)) {
+        //    $contenido = fread($convertido);
+        //    echo $contenido;
+        //    
+        //}
+        //fclose($archivo);
+
+
+
+        //if (isset($_GET['id'])) {
+        //    $respuesta = new select($_GET['id']);
+        //    $conexion = new conexion();
+        //    $resultado = mysqli_query($conexion->EstablecerConexion(), $respuesta->query());
+        //    while ($rows = mysqli_fetch_assoc($resultado)) {
+        //        $userData = $rows;
+        //    }
+        //    if (empty($userData)) {
+        //        echo 'no existe';
+        //        http_response_code(404);
+        //    } else {
+        //        echo json_encode($userData);
+        //    }
+        //} else {
+        //    $respuesta = new select($_GET['id'] = null);
+        //    $conexion = new conexion();
+        //    $resultado = mysqli_query($conexion->EstablecerConexion(), $respuesta->sinid());
+        //    $i = 0;
+        //    while ($rows = mysqli_fetch_assoc($resultado)) {
+        //        $userData[$i]['id'] = $rows['id'];
+        //        $userData[$i]['nombre'] = $rows['nombre'];
+        //        $userData[$i]['apellido'] = $rows['apellido'];
+        //        $userData[$i]['telefono'] = $rows['telefono'];
+        //        $i++;
+        //    }
+        //    if (empty($userData)) {
+        //        echo "No Existen datos";
+        //        http_response_code(404);
+        //    }else {
+        //        echo json_encode($userData, JSON_PRETTY_PRINT);
+        //    }
+        //}
         break;
     default:
         echo "Metodo no permitido";
